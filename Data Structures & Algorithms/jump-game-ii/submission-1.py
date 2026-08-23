742c21f90a72fdb91221d08d6acbeb38685e1b31
YJProjects <96485213+YJProjects@users.noreply.github.com>
2026-08-23 02:22:55 -0700

    Add: jump-game-ii - submission-1

---
+++ Data Structures & Algorithms/jump-game-ii/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        # Greedy
+
+        goal = len(nums) - 1
+        step_count = 0
+
+        while goal != 0:
+            new_idx = -1
+            for idx in range(goal-1, -1, -1):
+                if nums[idx] + idx >= goal:
+                    new_idx = idx
+
+            goal = new_idx
+            step_count += 1
+
+        return step_count
