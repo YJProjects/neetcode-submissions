59d956d8d38cf1d46d0897c1605329ab879f5986
YJProjects <96485213+YJProjects@users.noreply.github.com>
2026-09-03 20:47:48 -0400

    Add: longest-increasing-path-in-matrix - submission-2

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-2.py
@@ -0,0 +1,32 @@
+class Solution:
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        cache = {}
+        res = 0
+
+        def dfs(i, j, prev):
+            cords = (i, j)
+            if not (0 <= i < len(matrix)): # Invalid x cord
+                return 0
+            if not (0 <= j < len(matrix[i])): # Invalid y cord
+                return 0
+            if matrix[i][j] <= prev:
+                return 0
+            if cords in cache:
+                return cache[cords]
+
+            max_path = 1 + max(
+                dfs(i + 1, j, matrix[i][j]),
+                dfs(i - 1, j, matrix[i][j]),
+                dfs(i, j + 1, matrix[i][j]),
+                dfs(i, j - 1, matrix[i][j])
+            )
+
+            cache[cords] = max_path
+            return max_path
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                res = max(res, dfs(i, j, -1))
+
+        return res
+                
